82e8083c7898a82834fa4b0a47443bfaefa86e15
Matthias Mair <code@mjmair.com>
2022-08-31 19:49:01 +0200

    add mdl for testing

---
+++ src/inventree_digikey/models.py
@@ -0,0 +1,8 @@
+from operator import mod
+from django.db import models
+
+
+class TestMdl(models.Model):
+
+    test = models.CharField('abc', max_length=255)
+    bb = models.CharField('abc', max_length=255, null=True)
