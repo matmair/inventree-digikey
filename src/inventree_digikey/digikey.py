5ae5ce72073417962c414d9ea8b0a66a53465f0b
Matthias Mair <code@mjmair.com>
2022-08-31 19:46:41 +0200

    add pyproject

---
+++ src/inventree_digikey/digikey.py
@@ -0,0 +1,10 @@
+"""Digikey integration for InvenTree."""
+
+from plugin import InvenTreePlugin
+
+
+class DigikeyPlugin(InvenTreePlugin):
+
+    NAME = "Digikey Supplier Integration"
+    SLUG = "digikey"
+    TITLE = "Digikey integration"
